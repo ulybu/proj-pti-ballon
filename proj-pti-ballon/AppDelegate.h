e0f95f8b1fef21c0b9c5572de218f32820042fa3
ulysse <eleves@Mac-mini-de-ISMIN-9.local>
2012-10-03 16:35:03 +0200

    Initial Commit

---
+++ proj-pti-ballon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  proj-pti-ballon
+//
+//  Created by Élèves on 03/10/12.
+//  Copyright (c) 2012 Élèves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
